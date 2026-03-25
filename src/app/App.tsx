5e9af157e9af5ea2154366edaf6f91a7a9b30d2a
윤다인 <yundain@yundain-ui-MacBookAir.local>
2026-03-25 17:34:38 +0900

    feat: ProjectCard 컴포넌트 구현 및 스타일 적용

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
-import { Outlet } from 'react-router-dom';
+import ProjectCard from '../components/ProjectCard'
 
-export default function App() {
+function App() {
   return (
-   <div className="w-[1280px] h-[3628px] mx-auto bg-gray-50 overflow-hidden">
-      <main className="mx-auto max-w-5xl">
-        <Outlet />
-      </main>
+    <div style={{ padding: '40px' }}>
+      <ProjectCard />
     </div>
-  );
+  )
 }
+
+export default App
